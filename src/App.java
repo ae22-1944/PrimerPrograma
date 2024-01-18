3fdc3b0dc5c8c8e2a08f2570b315c15b5a1e5050
ae22-1944 <ae22-1944@unphu.edu.do>
2024-01-18 08:46:00 -0400

    Primer programa

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        String nombre = "Adán";
+        double monto = 475.50;
+        double resultado = (monto * 0.15) + monto;
+
+        System.out.println("Bienvenido!, mi nombre es " + nombre + " y el resultado es: " + resultado);
+    }
+}
